43b25b87122b7e15486c5e8d89aa55e4855f3d91
Ricardo <ricardocg.975@hotmail.com>
2021-07-29 18:03:15 -0500

    Se agrega proyecto de api

---
+++ API-Rest-Challenge/API-suggestions/Controllers/SuggestionsController.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLayer;
+using DataModel;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_suggestions.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SuggestionsController : Controller
+    {
+        [HttpGet]
+        public ActionResult<List<Citie>> Get()
+        {
+
+            string error = "";
+            bool res = false;
+
+            List<Citie> cities = ProcessCities.getCities("q=Beau&lat=53.35013&long=-113.41871", out res, out error);
+            return cities;
+        }
+    }
+}
